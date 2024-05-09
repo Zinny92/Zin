020e03470d685d99716dc8789f0c4cdb43ba9d89
Zinny92 <zinphuehlaing@gmail.com>
2024-05-09 09:48:37 +0700

    new commit

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    related_link = models.URLField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='blog')
+    date_time = models.DateTimeField()
+    publish = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
